fa3590ce5817a88976a10df4125aeea10747fd79
Ecamien <Ecamien@THINK-YDM>
2022-12-16 13:11:45 +0100

    syllabus update

--- src/pages/Syllabus.tsx
+++ src/pages/Syllabus.tsx
@@ -13,51 +13,41 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Button } from '@mui/material';
+import { Button, List,ListItem,ListItemButton,ListItemText, Drawer, IconButton } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 
 interface Data {
   name: string;
   Year:string;
   teacher:string;
+  price:number ;
 }
 
-
+//This data should be fetched from the Database in the future
 const API_DATA = [
-  { name:'LOL', Year: "2BA", teacher: "HIL" },
-  { name:'LLO', Year: "5MA", teacher: "MLT" },
-  { name:'OLL', Year: "1BA", teacher: "J3L" },
+  { name:'MATH', Year: "2BA", teacher: "HIL", price:25 },
+  { name:'GESTION', Year: "5MA", teacher: "MLT",price:10 },
+  { name:'INFO', Year: "1BA", teacher: "J3L",price:5 },
+  { name:'1', Year: "2BA", teacher: "HIL", price:25 },
+  { name:'2', Year: "5MA", teacher: "MLT",price:10 },
+  { name:'3', Year: "1BA", teacher: "J3L",price:5 },
+
+  { name:'4', Year: "2BA", teacher: "HIL", price:22 },
+  { name:'5', Year: "5MA", teacher: "MLT",price:101 },
+  { name:'6', Year: "1BA", teacher: "J3L",price:5 },
+  { name:'7', Year: "2BA", teacher: "HIL", price:25 },
+  { name:'8', Year: "5MA", teacher: "MLT",price:10 },
+  { name:'9', Year: "1BA", teacher: "J3L",price:5 },
+  { name:'10', Year: "2BA", teacher: "HIL", price:25 },
+
 ];
 
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
 
 type Order = 'asc' | 'desc';
 
-// function getComparator<Key extends keyof any>(
-//   order: Order,
-//   orderBy: Key,
-// ): (
-//   a: { [key in Key]: number | string },
-//   b: { [key in Key]: number | string },
-// ) => string {
-//   return order === 'desc'
-//     ? (a, b) => descendingComparator(a, b, orderBy)
-//     : (a, b) => -descendingComparator(a, b, orderBy);
-// }
 
 
 interface HeadCell {
@@ -86,6 +76,12 @@ const headCells: readonly HeadCell[] = [
     disablePadding: false,
     label: 'teacher',
   },
+  {
+    id: 'price',
+    numeric: true,
+    disablePadding: false,
+    label: 'price',
+  },
   
 ];
 
@@ -98,7 +94,7 @@ interface EnhancedTableProps {
   rowCount: number;
 }
 
-
+//Table additional functions
 function EnhancedTableHead(props: EnhancedTableProps) {
   const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
     props;
@@ -154,11 +150,6 @@ interface EnhancedTableToolbarProps {
 //Top of the screen
 function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
   const { numSelected } = props;
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
-  const handleDeleteSelect = (event: React.MouseEvent<unknown>) => {
-    setSelected([]);
-    console.log(selected);
-  }
   return (
     <Toolbar
       sx={{
@@ -189,21 +180,6 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
           Syllabus
         </Typography>
       )}
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton
-              onClick={(event) => handleDeleteSelect(event)}
-              >
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
     </Toolbar>
   );
 }
@@ -212,9 +188,10 @@ export default function SyllabusPage() {
   const [order, setOrder] = React.useState<Order>('asc');
   const [orderBy, setOrderBy] = React.useState<keyof Data>('Year');
   const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [selected, setSelected] = React.useState<readonly string[]>([]);
+  const [cart, setCart] = React.useState<readonly Data[]>([]);
+  const [isOpen, setIsOpen]= React.useState(false);
 
   const handleRequestSort = (
     event: React.MouseEvent<unknown>,
@@ -227,7 +204,7 @@ export default function SyllabusPage() {
 
   const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
-      const newSelected = API_DATA.map((n) => n.name);
+      const newSelected = tableData.map((n) => (n.name));
       setSelected(newSelected);
       return;
     }
@@ -257,6 +234,7 @@ export default function SyllabusPage() {
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
+    console.log(newPage,page)
   };
 
   const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -266,13 +244,11 @@ export default function SyllabusPage() {
 
 
   const isSelected = (name: string) => selected.indexOf(name) !== -1;
-
+  const [tableData, setTableData] = React.useState(API_DATA);
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - API_DATA.length) : 0;
-
-
-  const [tableData, setTableData] = React.useState(API_DATA);
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - tableData.length) : 0;
+  //Filter the table with dropdown
   const [selection, setSelection] = React.useState("");
   function handleChange(event: SelectChangeEvent) {
     setSelection(event.target.value);
@@ -281,29 +257,59 @@ export default function SyllabusPage() {
       ? API_DATA.filter(r => r.Year === event.target.value)
       : API_DATA;
     setTableData(_vals);
+    setSelected([]);
     console.log("tbs",tableData)
   }
+  //Adding elements to cart
+  let inCart:string[]=[]
+  function addCart(){
+    cart.map(item=>{
+      inCart= inCart.concat(item.name)
+    })
+    selected.map(row=>
+      {
+        var x:Data=API_DATA.filter(r=>r.name===row)[0]
+        if(cart && cart.length>0){
+          cart.map(item=>{
+          if (inCart.includes(row)){ //item is in cart
+            console.log('item',item)}
+            else{ //item not in cart
+              console.log('selected',row,'item',item)
+              setCart(cart.concat(x));
+            }
+          })
+        }
+        else {
+          console.log('row',row)
+          setCart(cart.concat(x));
+        }   
+    })
+  }
   return (  
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
+    <Box display="flex" 
+    alignItems="center"
+    justifyContent="center"
+    sx={{ width: '100%' }}>
+      <Paper sx={{ width: '50%', mb: 2 }}>
         <EnhancedTableToolbar numSelected={selected.length} />
         <TableContainer>
         <Select
-        style={{ width: "30%" }}
+        style={{ width: "20%" }}
         value={selection}
         onChange={handleChange}
         name="Year"
         displayEmpty
         >
-          <MenuItem value={""}>Tout les syllabus</MenuItem>
+          <MenuItem value={"" }>ALL</MenuItem>
           <MenuItem value={"2BA"}>2BA</MenuItem>
           <MenuItem value={"3BA"}>3BA</MenuItem>
           <MenuItem value={"5MA"}>5MA</MenuItem>
+          {/*Add Elements to dropdown here */}
         </Select>
           <Table
             sx={{ minWidth: 750 }}
             aria-labelledby="tableTitle"
-            size={dense ? 'small' : 'medium'}
+            size={'medium'}
           >
             <EnhancedTableHead
               numSelected={selected.length}
@@ -349,13 +355,15 @@ export default function SyllabusPage() {
                       </TableCell>
                       <TableCell align="right">{row.Year}</TableCell>
                       <TableCell align="right">{row.teacher}</TableCell>
+                      <TableCell align="right">{row.price}</TableCell>
+                      {/*Add Column to table here */}
                     </TableRow>
                   );
                 })}
               {emptyRows > 0 && (
                 <TableRow
                   style={{
-                    height: (dense ? 33 : 53) * emptyRows,
+                    height: (53) * emptyRows,
                   }}
                 >
                   <TableCell colSpan={6} />
@@ -375,8 +383,21 @@ export default function SyllabusPage() {
         />
       </Paper>
       <Button onClick={() => {
-        alert(selected);
+        addCart();
       }}>Add to Cart </Button>
+      <IconButton onClick={()=>setIsOpen(true)}>
+        <ShoppingCartIcon/>
+      </IconButton>
+      <Drawer 
+        anchor='right' 
+        open={isOpen}
+        onClose={()=>setIsOpen(false)}><List>
+        {cart.map((items,i)=><ListItem disablePadding key={items.name}>
+            <ListItemButton>
+              <ListItemText primary= {items.name+"    Price: "+items.price} />
+            </ListItemButton>
+          </ListItem>)}
+      </List></Drawer>
     </Box>
   );
 }
