db004974d51cbf95a22803760d0beb0f593ceea8
Ecamien <Ecamien@THINK-YDM>
2023-01-14 17:54:34 +0100

    add pop-up functionnality to Calendar

--- src/pages/Calendar/views/Calendar.tsx
+++ src/pages/Calendar/views/Calendar.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {Card, Table} from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 
+// Pop-up imports
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
 import DayColumn from './DayColumn';
 import WeekList from '../models/WeekList';
 import EcamCourse from '../models/EcamCourse';
@@ -9,37 +13,58 @@ import EcamCourse from '../models/EcamCourse';
 import './calendar.css';
 
 /// Used to define which "props" type is used by "CalendarView"
+/// Props are readonly
 interface CalendarViewProps<T extends EcamCourse> {
     courses?: T[];
+}
+
+/// Used to define the state's variables
+/// The state can be modified, with the "setState" method which triggers the render
+interface CalendarViewState<T extends EcamCourse> {
+    selectedCourse?: T;
 }
 
 /*
 The Calendar class
 The render() method return a table subdivised in 7 days (see "DayColumn.tsx"), plus 1 column used for each hour
 */
-class CalendarView extends React.Component<CalendarViewProps<EcamCourse>> {
-    columns: ColumnsType<WeekList> = [
-        {
-            title: 'Timecode',
-            dataIndex: 'timecode',
-            align: 'right',
-            width: 1,
-            render: (num: number) => <a>{num + "h^"}</a>,
-        },
-        new DayColumn("Monday"),
-        new DayColumn("Tuesday"),
-        new DayColumn("Wednesday"),
-        new DayColumn("Thursday"),
-        new DayColumn("Friday"),
-        new DayColumn("Saturday"),
-        new DayColumn("Sunday")
-    ];
+class CalendarView extends React.Component<
+    CalendarViewProps<EcamCourse>,
+    CalendarViewState<EcamCourse>
+> {
+    constructor(props: CalendarViewProps<EcamCourse>) {
+        super(props);
+        this.state = {selectedCourse: undefined};
+        this.handleSelection = this.handleSelection.bind(this);
+    }
 
-    constructor({courses = []}: CalendarViewProps<EcamCourse>) {
-        super({courses});
+    handleSelection(course: EcamCourse) {
+        this.setState({selectedCourse: course});
+    }
+
+    onClosedPopup() {
+        this.setState({selectedCourse: undefined});
     }
 
     render(): React.ReactNode {
+        let columns: ColumnsType<WeekList> = [
+            {
+                title: 'Timecode',
+                dataIndex: 'timecode',
+                align: 'right',
+                width: 1,
+                render: (num: number) => <a>{num + "h^"}</a>,
+            },
+            new DayColumn("Monday", this.handleSelection),
+            new DayColumn("Tuesday", this.handleSelection),
+            new DayColumn("Wednesday", this.handleSelection),
+            new DayColumn("Thursday", this.handleSelection),
+            new DayColumn("Friday", this.handleSelection),
+            new DayColumn("Saturday", this.handleSelection),
+            new DayColumn("Sunday", this.handleSelection)
+        ];
+        
+        // first line for all-day events
         let data: WeekList[] = [
             new WeekList("0", 0)
         ];
@@ -68,18 +93,29 @@ class CalendarView extends React.Component<CalendarViewProps<EcamCourse>> {
             data.push(w);
         }
 
-        // I wanted to use the "footer" propertie to display the details of a course that've been clicked by the user
-        // But you're free to use an other method to do so
+        // The pop-up can be set after the table without changing the rending
         return (
-            <Card>
-                <Table
-                    columns={this.columns}
-                    dataSource={data}
-                    pagination={false}
-                    footer={() => 'Footer'}
-                    bordered 
-                />
-            </Card>
+            <>
+                <Popup open={this.state.selectedCourse !== undefined} onClose={(e) => this.onClosedPopup()}>
+                    <ul>
+                        <li>{this.state.selectedCourse?.name ?? "No name"}</li>
+                        <li>{this.state.selectedCourse?.groupId ?? "Not provided"}</li>
+                        <li>De {this.state.selectedCourse?.starttime.getHours() ?? "??"}:{this.state.selectedCourse?.starttime.getMinutes() ?? "??"}h</li>
+                        <li>À {this.state.selectedCourse?.endtime.getHours() ?? "??"}:{this.state.selectedCourse?.endtime.getMinutes() ?? "??"}h</li>
+                        <li>Local : {this.state.selectedCourse?.local ?? "Not assigned"}</li>
+                        <li>{this.state.selectedCourse?.description ?? "No description provided"}</li>
+                    </ul>
+                </Popup>
+                <Card>
+                    <Table
+                        columns={columns}
+                        dataSource={data}
+                        pagination={false}
+                        footer={() => 'Footer'}
+                        bordered 
+                    />
+                </Card>
+            </>
         )
     }
 }
--- src/pages/Calendar/views/DayColumn.tsx
+++ src/pages/Calendar/views/DayColumn.tsx
@@ -2,50 +2,47 @@ import type { ColumnType } from 'antd/es/table';
 import { RenderedCell } from 'rc-table/lib/interface';
 import type { ReactNode } from 'react';
 
+import TimeSlot from './TimeSlot';
 import WeekList from '../models/WeekList';
-import './timeslot.css';
+import EcamCourse from '../models/EcamCourse';
+
+// Used to define the function type which take care of the selected event
+type SelectionHandler = (course: EcamCourse) => void;
 
 /// A class to describe each day of the week in the table
 /// To avoid repetitivity, because the render for each column is the same to display all the courses
 class DayColumn implements ColumnType<WeekList> {
     title: string;
+    // If you want to add properties for each column, there is the className
     className: string = "dayColumn";
     dataIndex: string;
+    onSelectedEvent: SelectionHandler;
 
-    constructor(title: string) {
+    constructor(title: string, onSelection: SelectionHandler) {
         this.title = title;
         this.dataIndex = title.toLowerCase();
+        this.onSelectedEvent = onSelection;
+    }
+
+    clickHandler(course: EcamCourse) {
+        this.onSelectedEvent(course);
     }
 
     // Render defined in these column used for each entry in the table
     render?: ((value: any, record: WeekList, index: number) => ReactNode | RenderedCell<WeekList>) | undefined = (_, record, index) => {
-        // console.log(record.getDay(this.dataIndex).length)
-        
         let courses = record.getDay(this.dataIndex);
         // When there are no courses for a day, do nothing
-        if (courses.length === 0) {
-            return
-        }
+        if (courses.length === 0) return
 
-        let course = courses[0];
-        let start_minutes = course.starttime.getMinutes();
-        let end_minutes = course.endtime.getMinutes();
-
-        // We can compute the relative block's length with the help of start and end times for each course
-        let length = course.endtime.getHours() - course.starttime.getHours() + (end_minutes - start_minutes)/60
-
-        // I use percentages to create a relative position from the top of each cell, and the length of each course
-        // zIndex is used to place later courses above the previous ones (for example, conflict with courses for different study year)
-        // I defined some background colors in "timeslot.css" to differentiate each study's option, and use the className propertie to choose which take
         return (
-            <div 
-                className={'timeslot ' + course.groupId.substring(1)}
-                style={{"top": (start_minutes*100/60).toString() + "%", "height": (length*100).toString() + "%", "zIndex": index}}
-            >
-                <a onClick={(event) => undefined}>{course.name}</a>
-            </div>
+            <>
+                <TimeSlot 
+                    courses={courses}
+                    rowIndex={index}
+                    clickHandler={this.onSelectedEvent}
+                />
+            </>
         );
-        // Just have to add an event to display all the details for a course when we click on it
     };
 }
 
